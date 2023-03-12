2822344c1046ad1c30ee03994c5a5d23f25608fb
Anuj0124Kumar <anuj0214@gmail.com>
2023-03-12 18:56:56 +0530

    start

---
+++ education_hub/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Education from './component/Education';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div>
+         <Navbar/>
+         <Education/>
+    </div>
+  );
+}
+
+export default App;
---
+++ education_hub/src/component/Education.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import EducationItem from "./EducationItem";
+
+export default function Education() {
+  return (
+    <>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <EducationItem />
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
